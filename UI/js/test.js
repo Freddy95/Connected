d80d92f23ccd18af734f7ce9ab48699ce07ebd43
Festevez <f.estevez95@gmail.com>
2016-11-22 21:47:08 -0500

    redirection

--- UI/js/test.js
+++ UI/js/test.js
@@ -3,7 +3,7 @@
 // 	    alert(data);
 // 	});
 
-// }	
+// }
 // function testpost(){
 // 	$.post("localhost:1337/posttest",{query:"SELECT * FROM ACCOUNTS"},function(data, status){
 // 	    alert(data);
@@ -16,24 +16,24 @@ function testget2(){
 	$.ajax({
 		type: "GET",
 	    url: "http://localhost:1337/gettest",
-	 
+
 	    // The name of the callback parameter, as specified by the YQL service
 	    jsonp: "callback",
-	 
+
 	    // Tell jQuery we're expecting JSONP
 	    dataType: "jsonp",
-	 
+
 	    // Tell YQL what we want and that we want JSON
 	    data: {
 	        q: "testig",
 	        format: "json"
 	    },
-	 
+
 	    // Work with the response
 	    success: function( response ) {
 	        console.log( response ); // server response
 	    }
-	});	
+	});
 
 }
 // this works
@@ -41,24 +41,26 @@ function testpost2(){
 	$.ajax({
 		type: "POST",
 	    url: "http://localhost:1337/posttest",
-	 
+
 	    // The name of the callback parameter, as specified by the YQL service
 	    jsonp: "callback",
-	 
+
 	    // Tell jQuery we're expecting JSONP
 	    dataType: "jsonp",
-	 
+
 	    // Tell YQL what we want and that we want JSON
 	    data: {
 	        sql_statement: "select * from Accounts",
 	        format: "json"
 	    },
-	 
+
 	    // Work with the response
 	    success: function( response ) {
-	        console.log( response ); // server response
+	        if(response === 1){
+						window.location = "login.html";
+					}
 	    }
-	});	
+	});
 
 }
 
@@ -66,23 +68,23 @@ function testquery(){
 	$.ajax({
 		type: "POST",
 	    url: "http://localhost:1337/query",
-	 
+
 	    // The name of the callback parameter, as specified by the YQL service
 	    json: "callback",
-	 
+
 	    // Tell jQuery we're expecting JSONP
 	    dataType: "jsonp",
-	 
+
 	    // Tell YQL what we want and that we want JSON
 	    data: {
 	        query: "SELECT * FROM Accounts",
 	        format: "json"
 	    },
-	 
+
 	    // Work with the response
 	    success: function( response ) {
 	        console.log( response ); // server response
 	    }
-	});	
+	});
 
 }
